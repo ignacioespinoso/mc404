bae6cf8c100e32891726a3d508b1e9ec8ba34eea
Ignacio Espinoso <ignacioespinoso@gmail.com>
2016-09-21 16:08:58 -0300

    Esboco da captura de entrada do programa

--- trabalho1/ias-as.c
+++ trabalho1/ias-as.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(int args, char *argv[]) {
+int main(int argc, char *argv[]) {
+    //Obtem o nome do arquivo de entrada
+    FILE *file = fopen(argv[1], "r");
 
+
+
+
+    //Fecha o arquivo de entrada caso o mesmo tenha sido aberto.
+    if(file) {
+        while(file != EOF) {
+
+        }
+        //Le o arquivo linha por linha
+        fclose(file);
+    }
+    return 0;
 }
