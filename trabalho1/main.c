e289444001a05d631eaa1c205cf8c3598430d22b
Ignacio Espinoso <ignacioespinoso@gmail.com>
2016-09-28 17:13:10 -0300

    Main agora lê linha por linha do arquivo.

--- trabalho1/main.c
+++ trabalho1/main.c
@@ -2,17 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
+//#include "*.h"
+
+#define MAX_SIZE 4097
+
 int main(int argc, char *argv[]) {
     //Obtem o nome do arquivo de entrada
     FILE *file = fopen(argv[1], "r");
-
-
-
+    char line[MAX_SIZE];
 
     //Fecha o arquivo de entrada caso o mesmo tenha sido aberto.
     if(file) {
-        while(file != EOF) {
-
+        while(fgets(line, sizeof(MAX_SIZE), file)) {
+            printf("%s", line);
         }
         //Le o arquivo linha por linha
         fclose(file);
