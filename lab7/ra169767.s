dc8ead2cdaa4eab5d7c8a7010a372cb0acdfced9
Ignacio Espinoso <ignacioespinoso@gmail.com>
2016-11-16 16:15:35 -0200

    Lab 7 Corrigido

--- lab7/ra169767.s
+++ lab7/ra169767.s
@@ -1,103 +1,93 @@
-@ Este codigo le um valor da entrada padrao, o multiplica por 2 e
-@ imprime o resultado na saida padrao.
-@ O valor a ser multiplicado sera recebido da entrada padrao na forma binaria.
-@ O valor binario devera conter EXATAMENTE 16 digitos.
-@   ex:  2  = 0000000000000010
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 @
-@ A resposta da entrada (o seu dobro) sera mostrado na saida padrao tambem no formato de
-@ uma string binaria de 16 digitos.
+@ Codigo para controle basico de um robo.
+@ Este codigo le os valores de 2 sonares frontais para decidir se o
+@ robo deve rotacionar
+@ 2 syscalls serao utilizadas para controlar o robo:
+@   write_motors  (syscall de numero 124)
+@                 Parametros:
+@                       r0 : velocidade para o motor 0  (valor de 6 bits)
+@                       r1 : velocidade para o motor 1  (valor de 6 bits)
 @
+@  read_sonar (syscall de numero 125)
+@                 Parametros:
+@                       r0 : identificador do sonar   (valor de 4 bits)
+@                 Retorno:
+@                       r0 : distancia capturada pelo sonar consultado (valor de 12 bits)
+@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-.text                         @ Muda o ponto de montagem para a secao de codigo (text)
+.text
+.align 4
+.globl _start
 
-.globl _start                 @ torna o rotulo _start global, ou seja, visivel pelo ligador.
+_start:                         @ main
 
-.align 4                      @ Alinha a posicao atual de montagem em um endereco multiplo de 4
-_start:                       @ ponto de entrada do programa.
+        mov r0, #0              @ Carrega em r0 a velocidade do motor 0.
+                                @ Lembre-se: apenas os 6 bits menos significativos
+                                @ serao utilizados.
+        mov r1, #0              @ Carrega em r1 a velocidade do motor 1.
+        mov r7, #124            @ Identifica a syscall 124 (write_motors).
+        svc 0x0                 @ Faz a chamada da syscall.
 
-@ -- le uma sequencia de 16 bytes da entrada padrao
-@ -- e armazena no buffer identificado pelo rotulo string.
-
-        mov r0, #0            @ carrega o valor 0 em r0, indicando que a entrada da
-                              @ syscall read sera em stdin
-        ldr r1, =string       @ carrega em r1 o endereco de um buffer na memoria.
-        mov r2, #17           @ carrega em r2 a quantidade maxima de dados (em bytes) a se lida.
-                              @ r0, r1 e r2 serao os argumentos da syscall read
-        mov r7, #3            @ carrega o valor 3 para r7, indicando o tipo da syscall (read)
-        svc 0x0               @ realiza uma chamada de sistema (syscall)
-
-@ -- converte a sequencia de caracteres lida em um numero na representacao sem sinal.
-
-        mov  r0, #0           @ para armazenar valor convertido
-        ldr  r1, =string      @ carrega em r1 o endereco da string
-        mov  r2, #0           @ iterador de loop
-
-loop_c:
-        mov  r0, r0, LSL #1   @ desloca o valor de r0 para esquerda
-                              @ LSB (bit menos significativo) de r0 recebe 0
-        ldrb r3, [r1, r2]     @ r3 = string[r2]
-        cmp  r3, #49          @ compara r3 com '1'  (49 em ASCII)
-        blt  skip             @ se for menor: r0 ja esta com 0 no LSB
-        orr  r0, r0, #1       @ senao: LSB de r0 recebe 1
-skip:
-        add r2, r2, #1        @ incrementa variavel de controle da iteracao
-        cmp r2, #16           @ compara r2 com o tamanho da string binaria
-        blt loop_c            @ se ainda esta com um valor menor que o tamanho da string
-
-@ -- Encontra o n-esimo numero da sequencia de fibonacci
-		@move a quantidade de iteracoes para o registrador r3
-		mov r3, r0
-		@r1 armazena o numero anterior, r0 o numero atual.
-		mov r0, #1
-		mov r1, #0
-loop_fibonacci:
-	cmp r3, #1		@Caso tenham sido feitas todas as iteracoes, sai do loop
-	bls fim_loop
-
-	mov r2, r0		@Salva o ultimo valor temporariamente em r2
-	add r0, r0, r1 	@Atualiza o ultimo valor
-	mov r1, r2		@Atualiza o valor anterior
-	sub r3, r3, #1 @Atualiza o iterador
-	b loop_fibonacci
-fim_loop:
-
-@ -- converte o resultado (em r0) em uma sequencia de caracteres '0' e '1' no buffer string.
-
-        ldr r1, =string       @ string  para preencher com string binaria
-        mov r2, #15           @ contador de iteracao
-        mov r3, #1            @ valor para shift
-
+        ldr r6, =1200           @ r6 <- 1200 (Limiar para rotacionar o robo)
+        ldr r8, =600            @ r8 <- 1200 (Limiar para rotacionar para a direita o robo)
 loop:
-        mov r4, #49           @ r4 = '1'
-        and r5, r0, r3        @ r5 = r0 AND r3
-        cmp r5, #0            @ compara r5 com 0 (zero)
-        bgt one               @ se for maior: r4 ja esta com '1'
-        mov r4, #48           @ senao: r4 = '0'
-one:
-        strb r4, [r1, r2]     @ string[r2] = r4
-        sub  r2, r2, #1       @ decrementa a variavel de controle do laco
-        mov  r3, r3, LSL #1   @ r3 = r3 << 1
-        cmp  r2, #0           @ compara r com zero
-        bge  loop             @ se ainda e maior ou igual, volta para o loop
+        mov r0, #3              @ Define em r0 o identificador do sonar a ser consultado.
+        mov r7, #125            @ Identifica a syscall 125 (read_sonar).
+        svc 0x0
+        mov r5, r0              @ Armazena o retorno da syscall.
 
-@ -- escreve a sequencia de 16 bytes na saida padrao.
-
-        mov r0, #1            @ carrega o valor 1 em r0, indicando que a saida da
-                              @ syscall write sera em stdout
-        ldr r1, =string       @ carrega em r1 o endereco da string
-        mov r2, #17           @ carrega em r2 o tamanho da string. r0,r1 e r2 serao
-                              @ os argumentos da syscall write
-        mov r7, #4            @ carrega o valor 4 para r7, indica o tipo da syscall
-        svc 0x0               @ realiza uma chamada de sistema (syscall)
-
-        mov r7, #1            @ carrega o valor 1 em r7, indicando a escolha da
-                              @ syscall exit
+        mov r0, #4              @ Define em r0 o sonar.
+        mov r7, #125
         svc 0x0
 
-.data @ Muda o ponto de montagem para a secao de dados
+        cmp r5, r0              @ Compara o retorno (em r0) com r5.
+        bge min                 @ Se r5 > r0: Salta pra min
+        mov r0, r5              @ Senao: r0 <- r5
+min:
 
 
-string:	.asciz "0000000000000000\n"  @ a diretiva .asciz coloca a cadeia
-	                             @ de caracteres especificada como parametro
-	                             @ na memoria e concatena o valor zero, indicando
-	                             @ o termino da cadeia.
+        cmp r0, r6              @ Compara r0 com r6
+        blt rotaciona_direita   @ Se r0 menor que o limiar: Rotaciona o robo
+
+                                @ Senao define uma velocidade para os 2 motores
+        mov r0, #50
+        mov r1, #50
+        mov r7, #124
+        svc 0x0
+
+        b loop                  @ Refaz toda a logica
+
+
+rotaciona_direita:              @ Roda para a direita caso o lado esteja livre
+
+        mov r0, #6              @ Define em r0 o identificador do sonar a ser consultado.
+        mov r7, #125            @ Identifica a syscall 125 (read_sonar).
+        svc 0x0
+        mov r5, r0              @ Armazena o retorno da syscall.
+
+        mov r0, #7              @ Define em r0 o sonar.
+        mov r7, #125
+        svc 0x0
+
+        cmp r5, r0              @ Armazena o menor valor em r0.
+        movlt r0, r5
+
+        cmp r0, r8              @ Compara o valor com o limiar.
+        blt rotaciona_esquerda  @ Roda para a esquerda caso esteja obstruido o lado direito.
+
+        mov r0, #0              @ Ajusta os motores para rodar o robo para a direita.
+        mov r1, #5
+        mov r7, #124
+        svc 0x0
+
+        b loop
+
+rotaciona_esquerda:
+        mov r0, #5              @ Ajusta os motores para rodar o robo para a esquerda.
+        mov r1, #0
+        mov r7, #124
+        svc 0x0
+
+        b loop
